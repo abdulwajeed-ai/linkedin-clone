12eb342fbd364b7e68b3580f24c6de3ed56cba5d
abdulwajeed-ai <abdulwajeed.ai.dev@gmail.com>
2025-07-15 15:56:09 +0530

    updating the navbar

--- frontend/src/components/Navbar-default/NavbarInitial.jsx
+++ frontend/src/components/Navbar-default/NavbarInitial.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const NavbarInitial = () => {
   return (
-    <div>NavbarInitial</div>
+    <nav className='w-[100%] bg-gray-100 md:px-[100px] px-[20px] flex justify-between py-4 box-border items-center'>
+      <div className='text-blue-600 font-bold text-3xl'>Linkedin</div>
+      <div className='flex gap-4'>
+        <div>Top content</div>
+        <div>People</div>
+        <div>Learning</div>
+        <div>Jobs</div>
+        <div>Games</div>
+      </div>
+      <div className='border px-3 py-1 rounded-full hover:font-bold '>Signup</div>
+    </nav>
   )
 }
 
